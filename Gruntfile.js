12de1a9211fa7975ad7ce959b248071ea4817da4
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-10-24 12:32:00 -0400

    rm dummy 'src' attributes of <img>s & squelch validator warning
    BS3 commits: 54cd0f8c2f1aaadefd8447f9ff4cd9bb04fe4e72

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,8 @@ module.exports = function(grunt) {
       options: {
         reset: true,
         relaxerror: [
-            "Bad value X-UA-Compatible for attribute http-equiv on element meta."
+            "Bad value X-UA-Compatible for attribute http-equiv on element meta.",
+            "Element img is missing required attribute src."
         ]
       },
       files: {
