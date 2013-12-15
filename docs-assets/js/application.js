b9fd054e59a4e0d2da168c74d4932ab873ea0d1d
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-12-15 17:50:23 -0500

    remove google styled list group js

--- docs-assets/js/application.js
+++ docs-assets/js/application.js
@@ -106,29 +106,6 @@
       minimumResultsForSearch: 20
       })
     })
-
-    // Google styled list group
-    $('.list-group-item > .dropdown').hide()
-
-    $('.list-group-item > .dropdown').on('hidden.bs.dropdown', function () {
-      $(this).hide()
-    })
-
-    $('li.list-group-item a, .list-group-item > .dropdown').on('mouseenter', function(event) {
-      if ( !$(this).is($('.dropdown.open')) ) {
-        $(this).parent().children('a').css('background-color', '#eee')
-      }
-      $(this).parent().children('.dropdown').show()
-    })
-
-    $('li.list-group-item a, .list-group-item > .dropdown').on('mouseleave', function() {
-      $(this).parent().children('a').removeAttr('style')
-      if ( $(this).parent().children('.dropdown').hasClass('open') ) {
-        $(this).parent().children('.dropdown').not('.open').hide()
-        return
-      }
-      $(this).parent().children('.dropdown').hide()
-    })
-})
+  })
 
 }(jQuery)
