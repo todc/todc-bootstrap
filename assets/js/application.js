77e95e5e2ed9d33f7670408f1159751f6519973b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-04 15:18:50 -0500

    regenerate the docs for v3.1.0-3.2.0

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,8 +3,8 @@
 // ++++++++++++++++++++++++++++++++++++++++++
 
 /*!
- * Copyright 2013 Twitter, Inc.
- *
+ * JavaScript for Bootstrap's docs (http://getbootstrap.com)
+ * Copyright 2011-2014 Twitter, Inc.
  * Licensed under the Creative Commons Attribution 3.0 Unported License. For
  * details, see http://creativecommons.org/licenses/by/3.0/.
  */
@@ -12,27 +12,26 @@
 
 !function ($) {
 
-  $(function(){
+  $(function () {
 
     // IE10 viewport hack for Surface/desktop Windows 8 bug
     //
     // See Getting Started docs for more information
     if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-      var msViewportStyle = document.createElement("style");
+      var msViewportStyle = document.createElement('style')
       msViewportStyle.appendChild(
         document.createTextNode(
-          "@-ms-viewport{width:auto!important}"
+          '@-ms-viewport{width:auto!important}'
         )
-      );
-      document.getElementsByTagName("head")[0].
-        appendChild(msViewportStyle);
+      )
+      document.querySelector('head').appendChild(msViewportStyle)
     }
 
 
     var $window = $(window)
     var $body   = $(document.body)
 
-    var navHeight = $('.navbar').outerHeight(true) + 10
+    var navHeight = $('.navbar').outerHeight(true) + 16
 
     $body.scrollspy({
       target: '.bs-sidebar',
@@ -59,8 +58,8 @@
             var navOuterHeight = $('.bs-docs-nav').height()
 
             return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-          }
-        , bottom: function () {
+          },
+          bottom: function () {
             return (this.bottom = $('.bs-footer').outerHeight(true))
           }
         }
@@ -73,24 +72,23 @@
 
     // tooltip demo
     $('.tooltip-demo').tooltip({
-      selector: "[data-toggle=tooltip]",
-      container: "body"
+      selector: '[data-toggle=tooltip]',
+      container: 'body'
     })
 
     $('.tooltip-test').tooltip()
     $('.popover-test').popover()
 
     $('.bs-docs-navbar').tooltip({
-      selector: "a[data-toggle=tooltip]",
-      container: ".bs-docs-navbar .nav"
+      selector: 'a[data-toggle=tooltip]',
+      container: '.bs-docs-navbar .nav'
     })
 
     // popover demo
-    $("[data-toggle=popover]")
-      .popover()
+    $('[data-toggle=popover]').popover()
 
     // button state demo
-    $('#fat-btn')
+    $('#loading-example-btn')
       .click(function () {
         var btn = $(this)
         btn.button('loading')
@@ -100,7 +98,7 @@
       })
 
     // Google styled selects
-    $("select").not('.input-lg').not('.input-sm').each(function() {
+    $('select').not('.input-lg').not('.input-sm').each(function () {
       var e = $(this)
       e.select2({
       minimumResultsForSearch: 20
