c994c682c70bc38f4512cd47f944841bd84269c0
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-09-01 13:13:30 -0400

    fixes #162
    thanks

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
     // Bootstrap variables
     bootstrapDir: 'bootstrap',
     bootstrapGit: 'https://github.com/twbs/bootstrap.git',
-    bootstrapVersion: 'v3.1.1',
+    bootstrapVersion: 'v3.2.0',
 
     // Task configuration.
     clean: {
