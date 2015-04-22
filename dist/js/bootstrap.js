c68b84552c747d6ad7b649587e31e06add1f968c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-22 10:45:36 -0400

    automatic `grunt dist`. automatic `grunt dist`
    BS3 commits: 48232aad6cdb5280e55e0aa0c42a4bb0d06ac03b,
    063c1b0780ea0240e4adce4c88d57fc23e099475

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1294,7 +1294,7 @@ if (typeof jQuery === 'undefined') {
     this.type      = type
     this.$element  = $(element)
     this.options   = this.getOptions(options)
-    this.$viewport = this.options.viewport && $(this.options.viewport.selector || this.options.viewport)
+    this.$viewport = this.options.viewport && $($.isFunction(this.options.viewport) ? this.options.viewport.call(this, this.$element) : (this.options.viewport.selector || this.options.viewport))
 
     if (this.$element[0] instanceof document.constructor && !this.options.selector) {
       throw new Error('`selector` option must be specified when initializing ' + this.type + ' on the window.document object!')
@@ -1429,6 +1429,7 @@ if (typeof jQuery === 'undefined') {
         .data('bs.' + this.type, this)
 
       this.options.container ? $tip.appendTo(this.options.container) : $tip.insertAfter(this.$element)
+      this.$element.trigger('inserted.bs.' + this.type)
 
       var pos          = this.getPosition()
       var actualWidth  = $tip[0].offsetWidth
