dc2bb42981968367e5110ec85beb09250c3db228
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-11-25 10:15:43 -0500

    grunt after version bump
    BS3 commits: 87121181c8a4b63192865587381d4b8ada8de30c

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
     // Bootstrap variables
     bootstrapDir: 'bootstrap',
     bootstrapGit: 'https://github.com/twbs/bootstrap.git',
-    bootstrapVersion: 'v3.3.5',
+    bootstrapVersion: 'v3.3.6',
 
     // Task configuration.
     clean: {
