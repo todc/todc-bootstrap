c06a9c93a783cb46ce7492721665a062b266e377
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-12-08 10:40:02 -0500

    Grunt: Factor out new docs-github task
    BS3 commits: 45b318b32f5161c32910d4cf4730200d62934ae4

--- Gruntfile.js
+++ Gruntfile.js
@@ -416,6 +416,7 @@ module.exports = function (grunt) {
   grunt.registerTask('docs-js', ['uglify:docsJs']);
   grunt.registerTask('lint-docs-js', ['jshint:assets', 'jscs:assets']);
   grunt.registerTask('docs', ['docs-css', 'lint-docs-css', 'docs-js', 'lint-docs-js', 'clean:docs', 'copy:docs']);
+  grunt.registerTask('docs-github', ['jekyll:github', 'htmlmin']);
 
-  grunt.registerTask('prep-release', ['dist', 'docs', 'jekyll:github', 'htmlmin', 'compress']);
+  grunt.registerTask('prep-release', ['dist', 'docs', 'docs-github', 'compress']);
 };
