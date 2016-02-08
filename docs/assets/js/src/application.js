8b3189e965dad8faa60536bd86c176c7e8b11f95
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-02-08 10:09:58 -0500

    docs/assets/js/src/application.js: Fix capitalization in comment
    BS3 commits: 38a70e4a275ef4d0c838c714b6e5e3270984dab4

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -59,7 +59,7 @@
       $('.bs-top').affix()
     }, 100)
 
-    // theme toggler
+    // Theme toggler
     ;(function () {
       var $stylesheetLink = $('#bs-theme-stylesheet')
       var $themeBtn = $('.bs-docs-theme-toggle')
