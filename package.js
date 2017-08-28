9e1a6a306fab9e54b6e560507b214d92cb5f9d79
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2017-08-28 09:36:59 -0400

    Rearrange all the docs to allow for a docs/major.minor/ setup
    BS4 commits: 3e76d6565603fafa2c85ad81d7b6345c4e279c72

---
+++ package.js
@@ -0,0 +1,20 @@
+// package metadata file for Meteor.js
+
+/* global Package:true */
+
+Package.describe({
+  name: 'todc:todc-bootstrap',  // https://atmospherejs.com/todc/todc-bootstrap
+  summary: 'Google-styled theme for Bootstrap.',
+  version: '4.0.0-beta-4.0.0-alpha',
+  git: 'https://github.com/todc/todc-bootstrap.git'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.0');
+  api.use('jquery', 'client');
+  api.addFiles([
+    'dist/css/bootstrap.css',
+    'dist/css/todc-bootstrap.css',
+    'dist/js/bootstrap.js'
+  ], 'client');
+});
