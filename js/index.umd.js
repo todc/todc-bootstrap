6060f9310ed191f838d175212d0e1b2015d16636
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-03-14 11:45:21 -0400

    build bootstrap in esm
    BS4 commits: 2fd50f98a53874d89aa60c9a698464ce1a0b7bfc

---
+++ js/index.umd.js
@@ -0,0 +1,32 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v4.3.1): index.umd.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from '../../bootstrap/js/src/alert'
+import Button from '../../bootstrap/js/src/button'
+import Carousel from '../../bootstrap/js/src/carousel'
+import Collapse from '../../bootstrap/js/src/collapse'
+import Dropdown from '../../bootstrap/js/src/dropdown'
+import Modal from '../../bootstrap/js/src/modal'
+import Popover from '../../bootstrap/js/src/popover'
+import ScrollSpy from '../../bootstrap/js/src/scrollspy'
+import Tab from '../../bootstrap/js/src/tab'
+import Toast from '../../bootstrap/js/src/toast'
+import Tooltip from '../../bootstrap/js/src/tooltip'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
