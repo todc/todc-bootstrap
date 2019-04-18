d6f0077b42101357b7c4065a93b71b9f0c31d553
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-04-18 09:00:29 -0400

    update paths

--- js/index.umd.js
+++ js/index.umd.js
@@ -5,17 +5,17 @@
  * --------------------------------------------------------------------------
  */
 
-import Alert from '../../bootstrap/js/src/alert'
-import Button from '../../bootstrap/js/src/button'
-import Carousel from '../../bootstrap/js/src/carousel'
-import Collapse from '../../bootstrap/js/src/collapse'
-import Dropdown from '../../bootstrap/js/src/dropdown'
-import Modal from '../../bootstrap/js/src/modal'
-import Popover from '../../bootstrap/js/src/popover'
-import ScrollSpy from '../../bootstrap/js/src/scrollspy'
-import Tab from '../../bootstrap/js/src/tab'
-import Toast from '../../bootstrap/js/src/toast'
-import Tooltip from '../../bootstrap/js/src/tooltip'
+import Alert from '../bootstrap/js/src/alert'
+import Button from '../bootstrap/js/src/button'
+import Carousel from '../bootstrap/js/src/carousel'
+import Collapse from '../bootstrap/js/src/collapse'
+import Dropdown from '../bootstrap/js/src/dropdown'
+import Modal from '../bootstrap/js/src/modal'
+import Popover from '../bootstrap/js/src/popover'
+import ScrollSpy from '../bootstrap/js/src/scrollspy'
+import Tab from '../bootstrap/js/src/tab'
+import Toast from '../bootstrap/js/src/toast'
+import Tooltip from '../bootstrap/js/src/tooltip'
 
 export default {
   Alert,
